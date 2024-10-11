ec5b19f0f81daf4f6b4c390a05761038c5f192f4
Luis Casanova <luiscasanovaguerrero@gmail.com>
2024-10-10 20:14:11 -0500

    primer repositorio taller2 Reactjs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   return(
     <>
         <Router>
-         
           <Routes>
             <Route path="/" element={<Main/>}/>
            <Route path="/rym" element={<RickAndMorty/>}/>
